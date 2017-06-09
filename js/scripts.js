60c75c49132b8fd8ea4d347281ba720ba570c2c1
Calla Rudolph <callarudolph@gmail.com>
2017-06-09 08:54:17 -0700

    initial js function to display input in html

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//business logic here:
+
+// user interface here:
+$(document).ready(function() {
+  $("form#pingpong").submit(function(event) {
+    event.preventDefault();
+    var input = parseInt($("input#number").val());
+    var result = play(result);
+
+    $("#result").append(result);
+  });
+});
