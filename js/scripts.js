b7a6e35f5eb086795d44e0f5d857e5cda4d7a9df
Calla Rudolph <callarudolph@gmail.com>
2017-06-09 11:10:52 -0700

    add else if statements to pop %5 to pong and %3 to ping

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,18 @@ function play(input) {
   for (var i = 1; i <= input; i += 1) {
     console.log(total)
     total.push("<li>" + i + "</li>");
-    if ((i % 5) === 0) {
+    if ((i % 15) === 0) {
       total.pop(i)
       total.push("<li>" + "ping-pong" + "</li>");
+    } else if ((i % 5) === 0) {
+      total.pop(i)
+      total.push("<li>" + "pong" + "</li>");
+    } else if ((i % 3) === 0) {
+      total.pop(i)
+      total.push("<li>" + "ping" + "</li>");
     }
-    }
+  }
   return total
-
 }
     // var fifteen = false;
     // console.log(fifteen)
