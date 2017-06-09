7f6dffeca06e30ed84b5e5cb45f0cc7be8112f3b
Calla Rudolph <callarudolph@gmail.com>
2017-06-09 10:12:33 -0700

    create for loop to return li of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 //business logic here:
 
+function play(input) {
+  var total = []
+  for (var i = 1; i <= input; i += 1) {
+    total.push("<li>" + i + "</li>");
+  }
+  return total
+}
+
 // user interface here:
 $(document).ready(function() {
   $("form#pingpong").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("input#number").val());
-    var result = play(result);
+    var result = play(input);
 
-    $("#result").append(result);
+    // console.log(input)
+
+    $("ul#result").append(result);
   });
 });
